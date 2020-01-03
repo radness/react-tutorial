016d174dde49c548308a7b7c3b33116e31994558
radness <yhs5128@gmail.com>
2020-01-03 23:53:33 +0900

    renderMovies

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,28 +2,69 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movieTitles = [
-  "Joker",
-  "Frozen",
-  "유열의 음악앨범",
-  "82년생 김지영",
-]
-
-const movieImages = [
-  "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F0357a82b7226464b87072c0b8d2246b71567986846719",
-  "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F5afd212b68e34e61a964d969dd898e2f1574298873981",
-  "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F582477f43a5143faa7a95290128596aa1565059580851",
-  "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fcc7165f75bb94140a95d977881cebc191571895256827"
-]
+// const movieTitles = []
+// const movieImages = []
 
 class App extends Component {
+
+  state = {
+
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      // state를 변경하면 render() 메소드가 새로운 state와 함께 다시 동작한다.
+      this.setState({
+        movies: [
+          {
+            id: "",
+            title: "Tunein for Love",
+            releaseDate: "2019.08.28",
+            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F582477f43a5143faa7a95290128596aa1565059580851"
+          },
+          {
+            id: "",
+            title: "Joker",
+            releaseDate: "2019.10.02",
+            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F0357a82b7226464b87072c0b8d2246b71567986846719"
+          },
+          {
+            id: "",
+            title: "KIM JI-YOUNG, BORN 1982",
+            releaseDate: "2019.10.23",
+            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fcc7165f75bb94140a95d977881cebc191571895256827"
+          },
+          {
+            id: "",
+            title: "Frozen 2",
+            releaseDate: "2019.11.21",
+            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F5afd212b68e34e61a964d969dd898e2f1574298873981"  
+          },
+          {
+            title: "Toy Stroy 4",
+            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fda2e6f0663514ba3aaf1f003733d08831560262646934",
+            releaseDate: "2019.06.20"
+          }
+        ]
+      })
+    }, 3000)
+  }
+ 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} releaseDate={movie.releaseDate} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {this.state.movies ? this._renderMovies() : 'Loaading'}
+        {/* Movie는 현 싸이클의 현재 엘리먼트를 의미 */}
+        {/* {this.state.movies.map((movie, index) => {
+          return <Movie title={movie.title} releaseDate={movie.releaseDate} poster={movie.poster} key={index} />
+        })} */}
       </div>
     );
   }
--- hello-react/src/Movie.js
+++ hello-react/src/Movie.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component {
+    
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired,
+        releaseDate: PropTypes.string.isRequired
+    }
+
     render() {
         // console.log(this.props);
         return (
@@ -14,10 +22,15 @@ class Movie extends Component {
 }
 
 class MoviePoster extends Component {
+    
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render() {
         // console.log(this.props);
         return (
-            <img src={this.props.poster} />
+            <img src={this.props.poster} alt="Movie Poster" />
         );
     }
 }

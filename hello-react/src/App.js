b38b503315c4841240879b60a0bad88504dbd22c
radness <yhs5128@gmail.com>
2020-01-05 23:44:04 +0900

    add movie genre

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,55 +6,79 @@ import Movie from './Movie.js';
 // const movieImages = []
 
 class App extends Component {
+  // Render: componentWillMount() -> render() ->  componentDidMount()
+  // Updete: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  state = {}
 
-  state = {
-
-  }
-  
   componentDidMount() {
-    setTimeout(() => {
-      // state를 변경하면 render() 메소드가 새로운 state와 함께 다시 동작한다.
-      this.setState({
-        movies: [
-          {
-            id: "",
-            title: "Tunein for Love",
-            releaseDate: "2019.08.28",
-            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F582477f43a5143faa7a95290128596aa1565059580851"
-          },
-          {
-            id: "",
-            title: "Joker",
-            releaseDate: "2019.10.02",
-            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F0357a82b7226464b87072c0b8d2246b71567986846719"
-          },
-          {
-            id: "",
-            title: "KIM JI-YOUNG, BORN 1982",
-            releaseDate: "2019.10.23",
-            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fcc7165f75bb94140a95d977881cebc191571895256827"
-          },
-          {
-            id: "",
-            title: "Frozen 2",
-            releaseDate: "2019.11.21",
-            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F5afd212b68e34e61a964d969dd898e2f1574298873981"  
-          },
-          {
-            title: "Toy Stroy 4",
-            poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fda2e6f0663514ba3aaf1f003733d08831560262646934",
-            releaseDate: "2019.06.20"
-          }
-        ]
-      })
-    }, 3000)
+    this._getMovies();
+    // fetch는 ajax를 url로 불러 올 수 있다.
+    
+    // setTimeout(() => {
+    //   // state를 변경하면 render() 메소드가 새로운 state와 함께 다시 동작한다.
+    //   this.setState({
+    //     movies: [
+    //       {
+    //         id: "",
+    //         title: "Tunein for Love",
+    //         releaseDate: "2019.08.28",
+    //         poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F582477f43a5143faa7a95290128596aa1565059580851"
+    //       },
+    //       {
+    //         id: "",
+    //         title: "Joker",
+    //         releaseDate: "2019.10.02",
+    //         poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F0357a82b7226464b87072c0b8d2246b71567986846719"
+    //       },
+    //       {
+    //         id: "",
+    //         title: "KIM JI-YOUNG, BORN 1982",
+    //         releaseDate: "2019.10.23",
+    //         poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fcc7165f75bb94140a95d977881cebc191571895256827"
+    //       },
+    //       {
+    //         id: "",
+    //         title: "Frozen 2",
+    //         releaseDate: "2019.11.21",
+    //         poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F5afd212b68e34e61a964d969dd898e2f1574298873981"  
+    //       },
+    //       {
+    //         title: "Toy Stroy 4",
+    //         poster: "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fmovie%2Fda2e6f0663514ba3aaf1f003733d08831560262646934",
+    //         releaseDate: "2019.06.20"
+    //       }
+    //     ]
+    //   })
+    // }, 3000)
   }
  
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} releaseDate={movie.releaseDate} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      // console.log(movie);
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
     return movies
+  }
+
+  // async: 순서와 상관없이 작업이 진행(비동기)
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
--- hello-react/src/Movie.js
+++ hello-react/src/Movie.js
@@ -1,38 +1,84 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-    
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-        releaseDate: PropTypes.string.isRequired
-    }
-
-    render() {
-        // console.log(this.props);
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
+function Movie({title, poster, genres, synopsis}) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                {/* alt : image tooltip을 위해서 alt 사용 */}
+                <MoviePoster poster={poster} alt={title} />
             </div>
-        );
-    }
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                {/* p = paragraph(문단의 약어) */}
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+function MoviePoster({poster, alt}) {
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
+}
+
+// crete new funcional component
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+    // releaseDate: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
+// class Movie extends Component {
+//     render() {
+//         // console.log(this.props);
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         );
+//     }
+// }
+
+// class MoviePoster extends Component {
     
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
 
-    render() {
-        // console.log(this.props);
-        return (
-            <img src={this.props.poster} alt="Movie Poster" />
-        );
-    }
-}
+//     render() {
+//         // console.log(this.props);
+//         return (
+//             <img src={this.props.poster} alt="Movie Poster" />
+//         );
+//     }
+// }
 
 export default Movie;
